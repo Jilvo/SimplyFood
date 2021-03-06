1e7a36c2643bd925916bc61dc0f0e39bb41c081b
Jilvo <jmillot41@gmail.com>
2021-03-06 18:37:07 +0100

    V0.4 Creation of a table for multiple research

--- README.md
+++ README.md
@@ -4,5 +4,11 @@
 ## Versioning :
 V0.1 Initialisation of the Project
 
+V0.2 Creation of home page , and account system
+
+V0.3 Research one recipe
+
+V0.4 Creation of a table for multiple research
+
 ## Author and Contribution :
 I built this program as part of an formation on OpenClassrooms. Therefore every pull request will be refused. Insted you can open an issue to signal a bug, a typographical error or just for give me an advice
--- recipe_list/static/js/scripts.js
+++ recipe_list/static/js/scripts.js
@@ -1,7 +1,6 @@
 function Add_Row(){
     console.log("La fonction est bien appelée")
-    $("#table_recipe").append("<tr><td>3</td><td><input class='form-control mr-sm-2' placeholder='Faire une recherche' name='query' id='query'></td><td><input type='text' id='name' name='name' requiredminlength='4' maxlength='60' size='40'></td</tr>"); 
-}
+    }
 
 // document.getElementById("demo").innerHTML =
 // obj.employees[1].firstName + " " + obj.employees[1].lastName;
