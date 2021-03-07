51e9873e0052171d4d5c6183a92eb12089d52576
Jilvo <jmillot41@gmail.com>
2021-03-07 16:18:40 +0100

    Fix the table, get row_id

--- recipe_list/static/js/scripts.js
+++ recipe_list/static/js/scripts.js
@@ -6,3 +6,12 @@ function Add_Row(){
 // obj.employees[1].firstName + " " + obj.employees[1].lastName;
 
 // console.log(JSON.stringify)
+
+// function Parse_json(){
+//    document
+//     var jsonData = JSON.parse(myMessage);
+//     for (var i = 0; i < jsonData.counters.length; i++) {
+//         var counter = jsonData.counters[i];
+//         console.log(counter.counter_name);
+//     }   
+// }
