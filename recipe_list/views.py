56b2e77f3411b0411edab40accde5c34f6075666
Jilvo <jmillot41@gmail.com>
2021-03-09 23:11:38 +0100

    send Json from Ajax to Django

--- recipe_list/views.py
+++ recipe_list/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import requests
 from google_trans_new import google_translator
 import os
+import json
 # Create your views here.
 
 def home_function(request):
@@ -12,39 +13,42 @@ def recipe_table(request):
 
 def find_ingredients(request):
     """search the ingredients of a recipe"""
-    API_FOOD_KEY = os.environ.get("API_FOOD_KEY")
-    query = request.GET.get("query")
+    # API_FOOD_KEY = os.environ.get("API_FOOD_KEY")
+    query = request.GET.get("json_list")
     print(query)
-    dict_recipe = {}
-    translate_query = google_translator().translate(query,lang_tgt='en')
-    print(translate_query)
-    url = "https://edamam-recipe-search.p.rapidapi.com/search"
+    # json_data = request.read()
+    # data = json.loads(query)
+    # print(data)
+    # dict_recipe = {}
+    # translate_query = google_translator().translate(query,lang_tgt='en')
+    # print(translate_query)
+    # url = "https://edamam-recipe-search.p.rapidapi.com/search"
 
-    querystring = {"q": translate_query}
-    headers = {
-        'x-rapidapi-key': API_FOOD_KEY,
-        'x-rapidapi-host': "edamam-recipe-search.p.rapidapi.com"
-        }
+    # querystring = {"q": translate_query}
+    # headers = {
+    #     'x-rapidapi-key': API_FOOD_KEY,
+    #     'x-rapidapi-host': "edamam-recipe-search.p.rapidapi.com"
+    #     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    # response = requests.request("GET", url, headers=headers, params=querystring)
     
-    recipe = response.json()["hits"]
-    for label in recipe:
-        recipe_dict = label["recipe"]
-        name_recipe = recipe_dict["label"]
-        ingredients = recipe_dict["ingredientLines"]
-        dict_recipe[name_recipe] = ingredients
-        print(name_recipe)
-        print(ingredients)
-        break
+    # recipe = response.json()["hits"]
+    # for label in recipe:
+    #     recipe_dict = label["recipe"]
+    #     name_recipe = recipe_dict["label"]
+    #     ingredients = recipe_dict["ingredientLines"]
+    #     dict_recipe[name_recipe] = ingredients
+    #     print(name_recipe)
+    #     print(ingredients)
+    #     break
 
-    translate_response = google_translator().translate(dict_recipe,lang_tgt='fr')
-    print(translate_response)
+    # translate_response = google_translator().translate(dict_recipe,lang_tgt='fr')
+    # print(translate_response)
 
-    context = {
-        "dict_recipe" : dict_recipe,
-        "translate_response" : translate_response,
+    # context = {
+    #     "dict_recipe" : dict_recipe,
+    #     "translate_response" : translate_response,
 
-    }
-    print(dict_recipe)
-    return render(request, "list.html", context)
+    # }
+    # print(dict_recipe)
+    return render(request, "list.html")
