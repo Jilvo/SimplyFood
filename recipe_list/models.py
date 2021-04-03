926d9d8d6ec33a983b04cc0af5866652f9096045
Jilvo <jmillot41@gmail.com>
2021-04-03 19:11:00 +0200

    Create 2 front for the index if the user is connected or not.

--- recipe_list/models.py
+++ recipe_list/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Recipe(models.Model):
     """Recipe class"""
+  
+    name = models.CharField(max_length=100, unique=True)
+    description_list = models.CharField(max_length=1000, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class User_Recipe_list(models.Model):
+    """Recipe_list class"""
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    list_recipe = models.CharField(max_length=200, unique=True)
+
+
+    def __str__(self):
+        return self.name
